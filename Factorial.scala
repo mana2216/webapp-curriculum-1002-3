39b96b8fee3c4def70b2ed4b62ea15ed3b723853
mana <ms00lx.221b@gmail.com>
2025-11-30 17:02:49 +0900

    末尾再帰最適化が行われる変更

--- Factorial.scala
+++ Factorial.scala
@@ -1,7 +1,9 @@
 import scala.math.BigInt
 
 object Factorial extends App {
-  def factorial(i: BigInt): BigInt = if (i == 0) 1 else i * factorial(i - 1)
+  val total = 0
+  def factorial(i: BigInt, x1: BigInt): BigInt = 
+    if (i == 0) x1 else factorial(i-1, x1 * i)
 
-  println(factorial(30000))
+  println(factorial(30000, 1))
 }
